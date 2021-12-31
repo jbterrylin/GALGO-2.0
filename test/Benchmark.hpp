561b7edee657dbfeb61607e163243f8cbe3c257c
Jbterrylin <wanglinlee1707@gmail.com>
2021-12-31 11:43:01 +0800

    update Benchmark

--- test/Benchmark.hpp
+++ test/Benchmark.hpp
@@ -3,34 +3,110 @@
 #ifndef BENCHMARK_HPP
 #define BENCHMARK_HPP
 
-//--------------------------
-// functions
-//--------------------------
-//
-// this example is from
-// https://en.wikipedia.org/wiki/Test_functions_for_optimization
-// Ackley's function:
-// f(x) = -20*exp(-0.2*sqrt(0.5*(x^2 + y^2))) - exp(0.5*(cos(2*pi*x) + cos(2*pi*y))) + exp(1) + 20
-// solution is: (0,0)
-//
 template <typename T>
-double Aaa(T x, T y)
+double sphere(std::vector< T > particle) 
 {
-    const double pi = 3.14159265358979323846;
+    double sum(0.);
 
-    double obj_val =  -20 * std::exp(-0.2*std::sqrt(0.5*(x*x + y*y))) - std::exp(0.5*(std::cos(2 * pi*x) + std::cos(2 * pi*y))) + std::exp(1) + 20;
-    return obj_val;
+    for (int i = 0; i < particle.size(); i++) {
+        sum += pow(particle[i], 2.);
+    }
+    return sum;
 }
 
 template <typename T>
-class AaaaObjective
+class SphereObjective
 {
 public:
     static std::vector<double> Objective(const std::vector<T>& x)
     {
-        double x0 = (double)x[0];
-        double x1 = (double)x[1];
-        double obj = -Aaa<double>(x0, x1);
+        std::vector<double> xd(x.size());
+        for (size_t i = 0; i < x.size(); i++) xd[i] = (double)x[i];
+
+        double obj = sphere<double>(xd);
+        return { obj };
+    }
+};
+
+template <typename T>
+double axisParallelHyperEllipsoid(std::vector< T > particle) 
+{
+    double sum(0.);
+
+    for (int i = 1; i <= particle.size(); i++) {
+        sum += (i * pow(particle[i], 2.));
+    }
+    return sum;
+}
+
+template <typename T>
+class AxisParallelHyperEllipsoidObjective
+{
+public:
+    static std::vector<double> Objective(const std::vector<T>& x)
+    {
+        std::vector<double> xd(x.size());
+        for (size_t i = 0; i < x.size(); i++) xd[i] = (double)x[i];
+
+        double obj = axisParallelHyperEllipsoid<double>(xd);
+        return { obj };
+    }
+};
+
+template <typename T>
+double rotatedHyperEllipsoid(std::vector< T > particle) 
+{
+    double sum(0.);
+
+    for (int i = 1; i <= particle.size(); i++) {
+        double inner (0.);
+        for (int j = 1; j <= i; j++)
+            inner += pow(particle[j],2);
+        sum += inner;
+    }
+
+    return sum;
+}
+
+template <typename T>
+class RotatedHyperEllipsoidObjective
+{
+public:
+    static std::vector<double> Objective(const std::vector<T>& x)
+    {
+        std::vector<double> xd(x.size());
+        for (size_t i = 0; i < x.size(); i++) xd[i] = (double)x[i];
+
+        double obj = rotatedHyperEllipsoid<double>(xd);
+        return { obj };
+    }
+};
+
+template <typename T>
+double rotatedHyperEllipsoid(std::vector< T > particle) 
+{
+    double sum(0.);
+
+    for (int i = 1; i <= particle.size(); i++) {
+        double inner (0.);
+        for (int j = 1; j <= i; j++)
+            inner += pow(particle[j],2);
+        sum += inner;
+    }
+
+    return sum;
+}
+
+template <typename T>
+class RotatedHyperEllipsoidObjective
+{
+public:
+    static std::vector<double> Objective(const std::vector<T>& x)
+    {
+        std::vector<double> xd(x.size());
+        for (size_t i = 0; i < x.size(); i++) xd[i] = (double)x[i];
+
+        double obj = rotatedHyperEllipsoid<double>(xd);
         return { obj };
     }
 };
