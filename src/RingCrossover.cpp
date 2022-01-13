06d8c0b04fbe4ae4dd07587224dc11a6b83926a7
Jbterrylin <wanglinlee1707@gmail.com>
2022-01-13 20:21:50 +0800

    add cpp file for each experiment, add looping function to repeat run GA

---
+++ src/RingCrossover.cpp
@@ -0,0 +1,78 @@
+//=================================================================================================
+//                  Copyright (C) 2018 Alain Lanthier - All Rights Reserved  
+//                  License: MIT License    See LICENSE.md for the full license.
+//                  Original code 2017 Olivier Mallet (MIT License)              
+//=================================================================================================
+
+#include "Galgo.hpp"
+
+#define TEST_INIT_POP
+#ifdef TEST_INIT_POP
+#include "../test/Classic/RingCrossoverFunctions.hpp"
+#endif
+
+// const int NBIT = 30;        // Has to remain between 1 and 64
+std::vector<galgo::Parameter<galgo::_TYPE, galgo::NBIT >> myvector;
+
+template <typename Z>  using FuncKT = std::vector<double>(*)(const std::vector<Z>&);
+
+template <typename _TYPE, typename T>
+void runGA(galgo::ConfigInfo<_TYPE>& config, FuncKT<T> Objective, std::string benchmarkName) {
+    bool resultToCsv = true;
+    std::cout << std::endl;
+    std::cout << config.csvFileName << "->" << benchmarkName;
+
+    config.Objective = SphereObjective<galgo::_TYPE>::Objective;
+
+    for(int i=0; i < 30; i++) {
+        myvector.clear();
+        for (int z = 0; z < 2; z++) myvector.push_back(galgo::Parameter<galgo::_TYPE, galgo::NBIT > ({ (galgo::_TYPE)-5.12, (galgo::_TYPE)5.12 }));
+        
+        galgo::GeneticAlgorithm<galgo::_TYPE> my_ga(config, myvector);
+        my_ga.resultToCsv = resultToCsv;
+        my_ga.csvFileName += "+" + benchmarkName;
+        my_ga.times = i;
+        my_ga.run();
+    }
+}
+
+template <typename _TYPE>
+void runFuntions(galgo::ConfigInfo<_TYPE>& config) {
+    runGA(config, SphereObjective<galgo::_TYPE>::Objective, "SphereObjective");
+    runGA(config, AxisParallelHyperEllipsoidObjective<galgo::_TYPE>::Objective, "AxisParallelHyperEllipsoidObjective");
+    runGA(config, RotatedHyperEllipsoidObjective<galgo::_TYPE>::Objective, "RotatedHyperEllipsoidObjective");
+    runGA(config, NormalizedSchwefelObjective<galgo::_TYPE>::Objective, "NormalizedSchwefelObjective");
+    runGA(config, GeneralizedRastriginObjective<galgo::_TYPE>::Objective, "GeneralizedRastriginObjective");
+    runGA(config, RosenbrocksValleyObjective<galgo::_TYPE>::Objective, "RosenbrocksValleyObjective");
+}
+
+int main()
+{
+#ifdef TEST_INIT_POP
+    // Test init initial population
+    {
+        // CONFIG
+        galgo::ConfigInfo<galgo::_TYPE> config;        // A new instance of config get initial defaults
+        set_config<galgo::_TYPE>(config);      // Override some defaults
+        set_RingCrossover<galgo::_TYPE>(config);
+        runFuntions(config);
+        set_SinglePointCrossover<galgo::_TYPE>(config);
+        runFuntions(config);
+        set_TwoPointCrossover<galgo::_TYPE>(config);
+        runFuntions(config);
+        set_HeuristicCrossover<galgo::_TYPE>(config);
+        runFuntions(config);
+        set_IntermediateCrossover<galgo::_TYPE>(config);
+        runFuntions(config);
+        set_ArithmeticCrossover<galgo::_TYPE>(config);
+        runFuntions(config);
+    }
+#endif
+
+#ifdef _WIN32
+	system("pause");
+#endif
+
+    return 0;
+}
+
---
+++ test/Classic/RingCrossoverFunctions.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#ifndef RINGCROSSOVERFUNCTION_HPP
+#define RINGCROSSOVERFUNCTION_HPP
+
+template <typename _TYPE>
+void set_config(galgo::ConfigInfo<_TYPE>& config)
+{
+    // override some defaults
+    config.mutinfo._sigma = 1.0;
+    config.mutinfo._sigma_lowest = 0.01;
+    config.mutinfo._ratio_boundary = 0.10;
+
+    config.covrate = 0.8;  // 0.0 if no cros-over
+    config.mutrate = 0.01;
+    config.recombination_ratio = 0.50;
+
+    config.elitpop = 0;
+    config.tntsize = 4;
+    config.Selection = StochasticUniformSelection; // TNT; //RWS
+    config.CrossOver = RingCrossover; //P1XO
+    config.isMultiCrossover = false;
+    config.mutinfo._type = galgo::MutationType::MutationGAM_sigma_adapting_per_mutation; //MutationSPM
+
+    config.popsize = 20;
+    config.nbgen = 10000 / config.popsize;
+    config.output = false;
+}
+
+template <typename _TYPE>
+void set_RingCrossover(galgo::ConfigInfo<_TYPE>& config)
+{
+    config.csvFileName = "RingCrossover";
+    config.CrossOver = RingCrossover; //P1XO
+}
+
+template <typename _TYPE>
+void set_SinglePointCrossover(galgo::ConfigInfo<_TYPE>& config)
+{
+    config.csvFileName = "P1XO";
+    config.CrossOver = P1XO; //P1XO
+}
+
+template <typename _TYPE>
+void set_TwoPointCrossover(galgo::ConfigInfo<_TYPE>& config)
+{
+    config.csvFileName = "P2XO";
+    config.CrossOver = P2XO; //P1XO
+}
+
+template <typename _TYPE>
+void set_HeuristicCrossover(galgo::ConfigInfo<_TYPE>& config)
+{
+    config.csvFileName = "HeuristicCrossover";
+    config.recombination_ratio = 1.2;
+    config.CrossOver = HeuristicCrossover; //P1XO
+}
+
+template <typename _TYPE>
+void set_IntermediateCrossover(galgo::ConfigInfo<_TYPE>& config)
+{
+    config.csvFileName = "IntermediateCrossover";
+    config.recombination_ratio = 0.5;
+    config.CrossOver = IntermediateCrossover; //P1XO
+}
+
+template <typename _TYPE>
+void set_ArithmeticCrossover(galgo::ConfigInfo<_TYPE>& config)
+{
+    config.csvFileName = "ArithmeticCrossover";
+    config.recombination_ratio = 0.5;
+    config.CrossOver = RealValuedWholeArithmeticRecombination; //P1XO
+}
+
+#endif
